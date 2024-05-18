2f4f378aec522fddd2931b44756cef6ab22ec250
pavlovw <133174113+pavlovw@users.noreply.github.com>
2024-05-18 18:56:37 -0400

    script.js
    se le ha agregado el despliegue de menu

--- Lex-Web/JavaScript/script.js
+++ Lex-Web/JavaScript/script.js
@@ -13,3 +13,46 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('scrolled');
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const btnMenu = document.getElementById('btn-menu');
+    const menuOverlay = document.getElementById('menu-overlay');
+    const menuIcon = btnMenu.querySelector('.menu-icon');
+    const closeIcon = document.createElement('span');
+    const navbar = document.getElementById('navbar');
+
+    closeIcon.innerHTML = '&times;';
+    closeIcon.classList.add('close-icon');
+
+    btnMenu.appendChild(closeIcon);
+
+    btnMenu.addEventListener('click', function () {
+        if (menuOverlay.classList.contains('open')) {
+            menuOverlay.classList.remove('open');
+            menuIcon.style.display = 'block';
+            closeIcon.style.display = 'none';
+            navbar.style.backgroundColor = '';
+            navbar.style.transition = 'none';
+        } else {
+            menuOverlay.classList.add('open');
+            menuIcon.style.display = 'none';
+            closeIcon.style.display = 'block';
+            navbar.style.backgroundColor = '#041f03';
+            navbar.style.transition = 'none';
+        }
+    });
+
+    menuOverlay.addEventListener('click', function (e) {
+        if (e.target === menuOverlay) {
+            menuOverlay.classList.remove('open');
+            menuIcon.style.display = 'block';
+            closeIcon.style.display = 'none';
+        }
+    });
+});
+
+
+
+
+
